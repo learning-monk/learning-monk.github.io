936ca07195527a984b7c4d6213ba98bc398cdac2
learning-monk <learningmonk888@gmail.com>
2019-09-02 20:57:16 +0530

    added sticky footer to all pages and fixed bugs

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+// Collapse navbar when navlink is clicked in responsive mode
+$(".navbar-nav>li>a").on("click", function() {
+  $(".navbar-collapse").collapse("hide");
+});
